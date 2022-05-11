0b40195014cc1efcf4663450136b8072779a2c9b
FelixBoegge <72453751+FelixBoegge@users.noreply.github.com>
2022-05-11 16:23:02 +0000

    implemented buttons for different algorithms, start and reset

--- pathfinder.py
+++ pathfinder.py
@@ -1,15 +1,17 @@
 import pygame
 import queue
 from queue import PriorityQueue
+from spot import Spot
+from pygame.locals import *
+import mazes
 
+pygame.init()
 
-SIZE = 600
-RIGHT_SPACE = 200
-WIN = pygame.display.set_mode((SIZE + RIGHT_SPACE, SIZE))
-pygame.display.set_caption('A* PathFinder')
 
 RED = (255, 0, 0)
+DARKRED = (180, 0, 0)
 GREEN = (0, 255, 0)
+DARKGREEN = (0, 180, 0)
 BLUE = (0, 255, 0)
 YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
@@ -17,78 +19,45 @@ BLACK = (0, 0, 0)
 PURPLE = (128, 0, 128)
 ORANGE = (255, 165, 0)
 GREY = (128, 128, 128)
+LIGHTGREY = (150, 150, 150)
 TURQUOISE = (64, 224, 208)
 
+SIZE = 600
+RIGHT_SPACE = 200
+WIN = pygame.display.set_mode((SIZE + RIGHT_SPACE, SIZE))
+pygame.display.set_caption('PathFinder')
 
-class Spot:
-    def __init__(self, row, col, size, total_rows):
-        self.row = row
-        self.col = col
-        self.x = row * size
-        self.y = col * size
-        self.color = WHITE
-        self.neighbors = []
-        self.size = size
-        self.total_rows = total_rows
+button_font = pygame.font.SysFont('Constantia', 20)
+record_font = pygame.font.SysFont('Areal', 15)
 
-    def get_pos(self):
-        return self.row, self.col
+class Button:
+    def __init__(self, x, y, width, height, color, color_hover, color_selected, label, selected = False):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.color_hover = color_hover
+        self.color_selected = color_selected
+        self.label = label
+        self.selected = selected
 
-    def is_blank(self):
-        return self.color == WHITE
 
-    def is_closed(self):
-        return self.color == RED
+    def draw_button(self, win):
+        pos = pygame.mouse.get_pos()
+        button_space = Rect(self.x, self.y, self.width, self.height)
 
-    def is_open(self):
-        return self.color == GREEN
+        if button_space.collidepoint(pos) and not self.selected:
+            pygame.draw.rect(win, self.color_hover, button_space)
+        else:
+            pygame.draw.rect(win, self.color, button_space)
+        if self.selected:
+            pygame.draw.rect(win, self.color_selected, button_space)
 
-    def is_barrier(self):
-        return self.color == BLACK
-
-    def is_start(self):
-        return self.color == ORANGE
-
-    def is_end(self):
-        return self.color == TURQUOISE
-
-    def reset(self):
-        self.color = WHITE
-
-    def make_closed(self):
-        self.color = RED
-
-    def make_open(self):
-        self.color = GREEN
-
-    def make_barrier(self):
-        self.color = BLACK
-
-    def make_start(self):
-        self.color = ORANGE
-
-    def make_end(self):
-        self.color = TURQUOISE
-
-    def make_path(self):
-        self.color = PURPLE
-
-    def draw(self, win):
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.size, self.size))
-
-    def update_neighbors(self, grid):
-        self.neighbors = []
-        if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier():# Down
-            self.neighbors.append(grid[self.row + 1][self.col])
-        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier():                  # Up
-            self.neighbors.append(grid[self.row - 1][self.col])
-        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier():# Right
-            self.neighbors.append(grid[self.row][self.col + 1])
-        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier():                  # Left
-            self.neighbors.append(grid[self.row][self.col - 1])
-
-    def __lt__(self, other):
-        return False
+        text = button_font.render(self.label, True, BLACK)
+        text_length = text.get_width()
+        text_height = text.get_height()
+        win.blit(text, (self.x + int(self.width / 2) - int(text_length / 2), self.y + int(self.height / 2) - int(text_height / 2)))
 
 
 def h(p1, p2):
@@ -105,6 +74,11 @@ def reconstruct_path(came_from, current, draw):
         length += 1
         draw()
     return length
+
+def print_record(win, path, steps):
+    win.blit(record_font.render('Steps: f{steps}', True, BLACK), (620, 240))
+    win.blit(record_font.render('Path: f{path}', True, BLACK), (620, 240))
+
 
 def a_star(draw, grid, start, end):
     count = 0
@@ -176,6 +150,7 @@ def BFS(draw, start, end):
             length = reconstruct_path(came_from, end, draw)
             start.make_start()
             end.make_end()
+            #print_record(WIN, length, steps)
             print(f"Steps: {steps}")
             print(f"Path length: {length}")
             return True
@@ -241,6 +216,7 @@ def make_grid(rows, size):
             grid[i].append(spot)
     return grid
 
+
 def draw_grid(win, rows, size):
     gap = size // rows
     for i in range(rows+1):
@@ -248,13 +224,16 @@ def draw_grid(win, rows, size):
         for j in range(rows+1):
             pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, size))
 
-def draw(win, grid, rows, size):
+
+def draw(win, grid, rows, size, buttons):
     win.fill(WHITE)
 
     for row in grid:
         for spot in row:
             spot.draw(win)
 
+    for button in buttons:
+        buttons[button].draw_button(win)
     draw_grid(win, rows, size)
     pygame.display.update()
 
@@ -267,14 +246,36 @@ def get_clicked_pos(pos, rows, size):
     return row, col
 
 
+def create_buttons():
+    buttons = {}
+    buttons['astar'] = Button(620, 30, 160, 50, GREY, LIGHTGREY, ORANGE, 'A* PathFinder')
+    buttons['BFS'] = Button(620, 90, 160, 50, GREY, LIGHTGREY, ORANGE, 'BFS PathFinder')
+    buttons['DFS'] = Button(620, 150, 160, 50, GREY, LIGHTGREY, ORANGE, 'DFS PathFinder')
+    buttons['start'] = Button(620, 520, 75, 50, DARKGREEN, GREEN, ORANGE, 'START')
+    buttons['reset'] = Button(705, 520, 75, 50, DARKRED, RED, ORANGE, 'RESET')
+    buttons['maze1'] = Button(620, 320, 160, 50, GREY, LIGHTGREY, ORANGE, 'I')
+    buttons['maze2'] = Button(620, 380, 160, 50, GREY, LIGHTGREY, ORANGE, 'II')
+    buttons['maze3'] = Button(620, 440, 160, 50, GREY, LIGHTGREY, ORANGE, 'III')
+    return buttons
+
+
+def create_maze(grid, maze):
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if maze[i][j] == "#":
+                grid[i][j].color = BLACK
+
+
+
 def main(win, size):
     ROWS = 50
     grid = make_grid(ROWS, size)
+    buttons = create_buttons()
     start = None
     end = None
     run= True
     while run:
-        draw(win, grid, ROWS, size)
+        draw(win, grid, ROWS, size, buttons)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -295,6 +296,73 @@ def main(win, size):
                     elif spot != end and spot != start:
                         spot.make_barrier()
 
+                elif pos[0] > buttons['astar'].x \
+                    and pos[0] < (buttons['astar'].x + buttons['astar'].width) \
+                    and pos[1] > buttons['astar'].y\
+                    and pos[1] < (buttons['astar'].y + buttons['astar'].height):
+                    buttons['astar'].selected = True
+                    buttons['BFS'].selected = False
+                    buttons['DFS'].selected = False
+
+                elif pos[0] > buttons['BFS'].x \
+                    and pos[0] < (buttons['BFS'].x + buttons['BFS'].width) \
+                    and pos[1] > buttons['BFS'].y\
+                    and pos[1] < (buttons['BFS'].y + buttons['BFS'].height):
+                    buttons['astar'].selected = False
+                    buttons['BFS'].selected = True
+                    buttons['DFS'].selected = False
+
+                elif pos[0] > buttons['DFS'].x \
+                    and pos[0] < (buttons['DFS'].x + buttons['DFS'].width) \
+                    and pos[1] > buttons['DFS'].y\
+                    and pos[1] < (buttons['DFS'].y + buttons['DFS'].height):
+                    buttons['astar'].selected = False
+                    buttons['BFS'].selected = False
+                    buttons['DFS'].selected = True
+
+                elif pos[0] > buttons['maze1'].x \
+                    and pos[0] < (buttons['maze1'].x + buttons['maze1'].width) \
+                    and pos[1] > buttons['maze1'].y \
+                    and pos[1] < (buttons['maze1'].y + buttons['maze1'].height):
+                    create_maze(grid, maze1)
+
+                elif pos[0] > buttons['maze2'].x \
+                    and pos[0] < (buttons['maze2'].x + buttons['maze2'].width) \
+                    and pos[1] > buttons['maze2'].y \
+                    and pos[1] < (buttons['maze2'].y + buttons['maze2'].height):
+                    pass
+
+                elif pos[0] > buttons['maze3'].x \
+                    and pos[0] < (buttons['maze3'].x + buttons['maze3'].width) \
+                    and pos[1] > buttons['maze3'].y \
+                    and pos[1] < (buttons['maze3'].y + buttons['maze3'].height):
+                    pass
+
+                elif start and end \
+                    and pos[0] > buttons['start'].x \
+                    and pos[0] < (buttons['start'].x + buttons['start'].width) \
+                    and pos[1] > buttons['start'].y\
+                    and pos[1] < (buttons['start'].y + buttons['start'].height):
+                    for row in grid:
+                        for spot in row:
+                            spot.update_neighbors(grid)
+
+                    if buttons['astar'].selected:
+                        a_star(lambda: draw(win, grid, ROWS, size, buttons), grid, start, end)
+                    if buttons['BFS'].selected:
+                        BFS(lambda: draw(win, grid, ROWS, size, buttons), start, end)
+                    if buttons['DFS'].selected:
+                        DFS(lambda: draw(win, grid, ROWS, size, buttons), start, end)
+
+                elif pos[0] > buttons['reset'].x \
+                    and pos[0] < (buttons['reset'].x + buttons['reset'].width) \
+                    and pos[1] > buttons['reset'].y \
+                    and pos[1] < (buttons['reset'].y + buttons['reset'].height):
+                    start = None
+                    end = None
+                    grid = make_grid(ROWS, size)
+
+
             elif pygame.mouse.get_pressed()[2]:
                 pos = pygame.mouse.get_pos()
                 if pos[0] < size and pos[1] < size:
@@ -305,21 +373,6 @@ def main(win, size):
                         start = None
                     elif spot == end:
                         end = None
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and start and end:
-                    for row in grid:
-                        for spot in row:
-                            spot.update_neighbors(grid)
-
-                    a_star(lambda: draw(win, grid, ROWS, size), grid, start, end)
-                    #BFS(lambda: draw(win, grid, ROWS, size), start, end)
-                    #DFS(lambda: draw(win, grid, ROWS, size), start, end)
-
-                if event.key == pygame.K_c:
-                    start = None
-                    end = None
-                    grid = make_grid(ROWS, size)
 
     pygame.quit()
 
